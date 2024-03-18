2ba0ec47ade36f3f14a16482c1e8e4519431e64e
John Doe <myehia162@gmail.com>
2024-03-18 10:29:04 +0200

    .

--- src/routes/topic.router.ts
+++ src/routes/topic.router.ts
@@ -22,73 +22,73 @@ const router: Router = express.Router();
  *       '500':
  *         description: Internal Server Error. Failed to retrieve topics.
  *
- *   post:
- *     summary: Add a new topic
- *     description: Adds a new topic.
- *     security:
- *       - jwt: []
- *     tags: [topics]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               topic_ar:
- *                 type: string
- *               topic_en:
- *                 type: string
- *               description_ar:
- *                 type: string
- *               description_en:
- *                 type: string
- *     responses:
- *       '201':
- *         description: Created. Topic successfully added.
- *       '400':
- *         description: Bad Request. Invalid request parameters.
- *       '401':
- *         description: Unauthorized. User authentication failed.
- *       '500':
- *         description: Internal Server Error. Failed to add the topic.
+//  *   post:
+//  *     summary: Add a new topic
+//  *     description: Adds a new topic.
+//  *     security:
+//  *       - jwt: []
+//  *     tags: [topics]
+//  *     requestBody:
+//  *       required: true
+//  *       content:
+//  *         application/json:
+//  *           schema:
+//  *             type: object
+//  *             properties:
+//  *               topic_ar:
+//  *                 type: string
+//  *               topic_en:
+//  *                 type: string
+//  *               description_ar:
+//  *                 type: string
+//  *               description_en:
+//  *                 type: string
+//  *     responses:
+//  *       '201':
+//  *         description: Created. Topic successfully added.
+//  *       '400':
+//  *         description: Bad Request. Invalid request parameters.
+//  *       '401':
+//  *         description: Unauthorized. User authentication failed.
+//  *       '500':
+//  *         description: Internal Server Error. Failed to add the topic.
  */
 router
   .route('/')
   .get(authController.requireAuth, topicsController.getTopics)
-  .post(authController.requireAuth, topicsController.addTopic);
+//   .post(authController.requireAuth, topicsController.addTopic);
 
-/**
- * @swagger
- * /topics/{topic}:
- *   delete:
- *     summary: Delete a topic
- *     description: Deletes a topic.
- *     security:
- *       - jwt: []
- *     tags: [topics]
- *     parameters:
- *       - in: path
- *         name: topic
- *         description: The name of the topic to delete.
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       '204':
- *         description: No Content. Topic successfully deleted.
- *       '400':
- *         description: Bad Request. Invalid request parameters.
- *       '401':
- *         description: Unauthorized. User authentication failed.
- *       '404':
- *         description: Not Found. Topic not found.
- *       '500':
- *         description: Internal Server Error. Failed to delete the topic.
- */
-router
-  .route('/:topic')
-  .delete(authController.requireAuth, topicsController.deleteTopic);
+// /**
+//  * @swagger
+//  * /topics/{topic}:
+//  *   delete:
+//  *     summary: Delete a topic
+//  *     description: Deletes a topic.
+//  *     security:
+//  *       - jwt: []
+//  *     tags: [topics]
+//  *     parameters:
+//  *       - in: path
+//  *         name: topic
+//  *         description: The name of the topic to delete.
+//  *         required: true
+//  *         schema:
+//  *           type: string
+//  *     responses:
+//  *       '204':
+//  *         description: No Content. Topic successfully deleted.
+//  *       '400':
+//  *         description: Bad Request. Invalid request parameters.
+//  *       '401':
+//  *         description: Unauthorized. User authentication failed.
+//  *       '404':
+//  *         description: Not Found. Topic not found.
+//  *       '500':
+//  *         description: Internal Server Error. Failed to delete the topic.
+//  */
+// router
+//   .route('/:topic')
+//   .delete(authController.requireAuth, topicsController.deleteTopic);
 
 /**
  * @swagger
